23f3d8cbbf82321254f799b2ab24593235ea6a04
Cedricaf <34737@ma-web.nl>
2022-11-30 15:26:53 +0100

    edits

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-let naam = "Leerling";
 
+
+
+class player {
+
+    constructor(index)
+    {
+        this.index = index;
+        this.atTile = 0;
+        this.pawn = document.getElementsByClassName("pawn" + index)[0];
+        this.pawn.style.display = block;
+
+    }
+
+}
+
+class tile {
+
+    constructor(div)
+    {
+        this.div = div;
+        this.goto = -1;
+    }
+
+}
+
+class game {
+    
+    constructor()
+}
 console.log(naam);
